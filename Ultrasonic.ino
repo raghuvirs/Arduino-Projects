5c5bc5ad849cc16e8c2983c93a3a447b4517d9b4
raghuvirs <107181721+raghuvirs@users.noreply.github.com>
2022-09-02 21:46:43 +0530

    Add files via upload

---
+++ Ultrasonic.ino
@@ -0,0 +1,24 @@
+#define echoPin 12
+#define trigPin 11
+
+long duration; 
+int distance; 
+
+void setup() {
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600); 
+  Serial.println("with Arduino UNO R3");
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delay(2000);
+  digitalWrite(trigPin, HIGH);
+  delay(2000);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+}
