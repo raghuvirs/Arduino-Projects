fc84b970fb02e80a0159dc939dd8c4f1fee71f08
Raghuvir Singh <107181721+raghuvirs@users.noreply.github.com>
2022-09-02 22:49:50 +0530

    Add files via upload

---
+++ PIR/PIR.ino
@@ -0,0 +1,32 @@
+int led = 13;                
+int pir = 11;              
+int state = LOW;             
+int val = 0;                 
+
+void setup() {
+  pinMode(led, OUTPUT);      
+  pinMode(pir, INPUT);    
+  Serial.begin(9600);        
+}
+
+void loop(){
+  val = digitalRead(pir); 
+  if (val == HIGH) {         
+    digitalWrite(led, HIGH); 
+    delay(500);               
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       
+    }
+  } 
+  else {
+      digitalWrite(led, LOW); 
+      delay(500);              
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       
+    }
+  }
+}
---
+++ PIR/README.txt
@@ -0,0 +1,3 @@
+This project includes basic working of PIR sensor using arduino and LED.
+PIR.INO contains the arduino code, PIR.fzz contains basic sketch of the project. 
+To open .fzz file, use Fritzing software.
